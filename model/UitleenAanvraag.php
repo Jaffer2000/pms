18002eb1720438976f24b1b951e5acd53b2dde1f
Jaffer <219638@student.scalda.nl>
2023-06-01 12:08:21 +0200

    last changes

--- model/UitleenAanvraag.php
+++ model/UitleenAanvraag.php
@@ -276,22 +276,5 @@ public function getStatusLabel($status)
         return '';
     }
 }
-    public function getAanvraagNaamList() {
-        $return_array = array();
-    
-            $query = "SeLeCt * FrOm `aanvragen` limit 1;";
-            $result = $this->connect()->query($query);
-            // For all database results:
-            foreach ($result as $idx => $array){
-                // Nieuw object
-                $lijst = new UitleenAanvraag();
-                // Set info
-                $lijst->setNaam($array['naamaanvraag']);
-    
-                // Add new object to return array.
-                $return_array[] = $lijst;
-            }
-            return $return_array;
-    }
 
 }
